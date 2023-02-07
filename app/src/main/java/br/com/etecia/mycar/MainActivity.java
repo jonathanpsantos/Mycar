694ef1f1274002b316dba233bee5c935a8957257
jonathanpsantos <jonathan.santos194@etec.sp.gov.br>
2023-02-07 20:25:50 -0300

    apresentando xml ao java

--- app/src/main/java/br/com/etecia/mycar/MainActivity.java
+++ app/src/main/java/br/com/etecia/mycar/MainActivity.java
@@ -1,14 +1,21 @@
 package br.com.etecia.mycar;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Apresentando o xml ao Java
+    ListView idlistValores ;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
