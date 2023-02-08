bc4408fd4e2c35cf2cf30bbba4a6746a613b0c1b
jonathanpsantos <jonathan.santos194@etec.sp.gov.br>
2023-02-07 21:23:08 -0300

    implementando a class

--- app/src/main/java/br/com/etecia/mycar/MainActivity.java
+++ app/src/main/java/br/com/etecia/mycar/MainActivity.java
@@ -1,21 +1,51 @@
 package br.com.etecia.mycar;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
     // Apresentando o xml ao Java
-    ListView idlistValores ;
-
-
+    ListView idlistValores;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        idlistValores = findViewById(R.id.idlistValores);
 
+        MyAdapter adapter = new MyAdapter();
+        idlistValores.setAdapter(adapter);
     }
+
+    public class MyAdapter extends BaseAdapter {
+
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
+    }
+
 }
